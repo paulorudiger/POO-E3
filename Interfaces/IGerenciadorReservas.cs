d2d0b89cfb4da14bb1b2820decf65ccee2ffb734
paulorudiger <paulo.e.rudiger@gmail.com>
2024-10-11 17:20:02 -0300

    Refatorada interface

---
+++ Interfaces/IGerenciadorReservas.cs
@@ -0,0 +1,17 @@
+﻿using POO_E2.Abstracoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_E2.Interfaces
+{
+    // Interface para definir os métodos de gerenciamento de reservas
+    public interface IGerenciadorReservas
+    {
+        void Reservar(Livro livro, Estudante estudante, Professor professor);
+        void CancelarReserva(Livro livro, Estudante estudante);
+        void RegistrarDevolucao(Livro livro, Estudante estudante);
+    }
+}
